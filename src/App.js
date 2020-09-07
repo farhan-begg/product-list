cca22164d27c0aae7652624a966f45ceec1ac175
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-09-07 11:17:27 -0700

    test

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import data, { allCategories, categoriesUnique, productCount } from './data.js';
 
 console.log(allCategories)
 
+const item = data.json
+
 function App() {
   return (
     <div className="App">
-
-
+          {/* <div key={item} className="products-header">
+          <h1 className="block">{item.name}</h1>
+          <h4 className="block">{item.price}</h4>
+          <h4 className="block">{item.category}</h4>
+        </div> */}
+  );
+}
+   
 
     </div>
   );
